21a679c7b2a53116077097e97553371f14e3f137
Josh Tynjala <joshtynjala@gmail.com>
2017-09-06 15:30:13 -0700

    added semantic-ui-css as node dependency instead of loading from CDN because that's best practice

--- loan-payment-calculator/src/index.tsx
+++ loan-payment-calculator/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
 	<App />,
